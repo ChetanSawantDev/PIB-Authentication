cd29b285cff44a8c548c609a8d88ead640c63348
ChetanSawant <chetansawant.dev@gmail.com>
2025-01-21 21:25:47 +0530

    First Commit

---
+++ src/main/java/com/pibauth/PibAuthenticationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pibauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PibAuthenticationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PibAuthenticationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pibauth/PibAuthenticationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pibauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PibAuthenticationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
